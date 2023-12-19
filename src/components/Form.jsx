65ef7bd7f8416d34bb33fcc183a8857c0b1f6a6d
Gayatri Ravindranath Batgeri <gayatribatgeri27@gmail.com>
2023-12-19 11:29:38 +0530

    updated form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -55,7 +55,7 @@ const Form = () => {
 
     try{
         //fetching API from backend
-        const response = await fetch('http://localhost:3307/api/register' , {
+        const response = await fetch('https://yoga-backend-t5rc.onrender.com/api/register' , {
             method:'POST',
             headers:{
                 'content-type':'application/json',
