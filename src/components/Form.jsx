b824199d18c40cb43d00502cd72ebaf5e74ea18d
Gayatri Ravindranath Batgeri <gayatribatgeri27@gmail.com>
2023-12-19 11:26:22 +0530

    final commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -55,7 +55,7 @@ const Form = () => {
 
     try{
         //fetching API from backend
-        const response = await fetch('https://yoga-registration-mvcs.onrender.com/api/register' , {
+        const response = await fetch('http://localhost:3307/api/register' , {
             method:'POST',
             headers:{
                 'content-type':'application/json',
